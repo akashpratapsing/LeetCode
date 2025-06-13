e65d86f946d07dd0fb6a2e56f47dfd01d476a77a
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-06-13 15:23:26 +0530

    Time: 0 ms (100%), Space: 41.4 MB (15.54%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return rob(nums, nums.length - 1, dp);
+        
+    }
+
+    public int rob(int[] nums, int idx, int[] dp){
+
+        if (idx == 0){
+            return nums[idx];
+        }
+
+        if (idx < 0){
+            return 0;
+        }
+
+        if (dp[idx] != -1){
+            return dp[idx];
+        }
+
+        int rob = nums[idx] + rob(nums, idx - 2, dp);
+        int dontRob = 0 + rob(nums, idx - 1, dp);
+
+        return dp[idx] = Math.max(rob, dontRob);
+    }
+}
