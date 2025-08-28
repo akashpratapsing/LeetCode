8936ee4672ea45bdd72e97fe96a1effb69f3077c
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-08-28 14:12:34 +0530

    Time: 10 ms (94.29%), Space: 44.9 MB (27.31%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -7,9 +7,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             lastSeen[s.charAt(i) - 'a'] = i;
-            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
-                count += (1 + Math.min(Math.min(lastSeen[0], lastSeen[1]), lastSeen[2]));
-            }
+            count += (1 + Math.min(Math.min(lastSeen[0], lastSeen[1]), lastSeen[2]));
         }
 
         return count;
