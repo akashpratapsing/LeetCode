9b1e4d3158084352ba779cd2770134174b214039
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-09-10 16:45:21 +0530

    Time: 1 ms (100%), Space: 46.4 MB (12.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,65 +1,47 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int n = nums1.length;
-        int m = nums2.length;
-        int x = n + m;
-        int index2 = x / 2;
-        int index1 = index2 - 1;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        int i = 0;
-        int j = 0;
-        int index = 0;
-        int ele2 = -1;
-        int ele1 = -1;
+        int start = 0, end = n1;
+        int n = n1 + n2;
+        int left = (n + 1) / 2;
 
-        while (i < n && j < m) {
-            if (nums1[i] <= nums2[j]) {
-                if (index == index2) {
-                    ele2 = nums1[i];
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+            int mid2 = left - mid;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if (mid < n1) {
+                r1 = nums1[mid];
+            }
+            if (mid2 < n2) {
+                r2 = nums2[mid2];
+            }
+            if (mid - 1 >= 0) {
+                l1 = nums1[mid - 1];
+            }
+            if (mid2 - 1 >= 0) {
+                l2 = nums2[mid2 - 1];
+            }
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 0) {
+                    return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+                } else {
+                    return (double) Math.max(l1, l2);
                 }
-                if (index == index1){
-                    ele1 = nums1[i];
-                }
-                index++;
-                i++;
+            } else if (l1 > r2) {
+                end = mid - 1;
             } else {
-                if (index == index2) {
-                    ele2 = nums2[j];
-                }
-                if (index == index1){
-                    ele1 = nums2[j];
-                }
-                index++;
-                j++;
+                start = mid + 1;
             }
         }
 
-        while (i < n) {
-            if (index == index2) {
-                ele2 = nums1[i];
-            }
-            if (index == index1){
-                ele1 = nums1[i];
-            }
-            index++;
-            i++;
-        }
-        while (j < m) {
-            if (index == index2) {
-                ele2 = nums2[j];
-            }
-            if (index == index1){
-                ele1 = nums2[j];
-            }
-            index++;
-            j++;
-        }
-
-        if (x % 2 == 0) {
-            return (double) (ele1 + ele2) / 2;
-        } else {
-            return (double) ele2;
-        }
+        return 0;
     }
 }
