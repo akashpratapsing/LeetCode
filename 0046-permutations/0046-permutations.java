795e55ef86086f2adf0c54da9a296c8850ca5fe3
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-09-18 16:55:41 +0530

    Time: 0 ms (100%), Space: 44.5 MB (82.52%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        boolean[] map = new boolean[nums.length];
-        permutations(0, nums, result, new ArrayList<>(), map);
+        permutations(0, nums, result);
         return result;
     }
 
-    public void permutations(int index, int[] nums, List<List<Integer>> result, List<Integer> list, boolean[] map){
-        if (list.size() == nums.length){
-            result.add(new ArrayList<>(list));
+    public void permutations(int index, int[] nums, List<List<Integer>> result) {
+        if (index == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                list.add(nums[i]);
+            }
+            result.add(list);
             return;
         }
 
-        for (int i = 0; i < nums.length; i++){
-            if (!map[i]){
-                list.add(nums[i]);
-                map[i] = true;
-                permutations(index + 1, nums, result, list, map);
-                list.remove(list.size() - 1);
-                map[i] = false;
-            }
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            permutations(index + 1, nums, result);
+            swap(i, index, nums);
         }
     }
 
-   
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
