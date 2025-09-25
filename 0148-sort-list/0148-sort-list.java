12161af84951781c9cf10895694302c8c1569076
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-09-25 21:19:55 +0530

    Time: 9 ms (96.36%), Space: 56.9 MB (55.9%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -14,7 +14,7 @@ class Solution {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode slow = head, fast = head.next.next;
+        ListNode slow = head, fast = head.next;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
