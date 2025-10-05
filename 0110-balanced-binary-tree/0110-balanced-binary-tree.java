0f2c7bbd0cdfc60c99417fbadb985a1d4caaf301
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-10-05 23:08:36 +0530

    Time: 0 ms (100%), Space: 44.4 MB (58.29%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -22,7 +22,7 @@ class Solution {
     public int getHeight(TreeNode root) {
 
         if (root == null){
-            return 1;
+            return 0;
         }
 
         int left = getHeight(root.left);
