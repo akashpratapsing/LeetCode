6e58289f42e27a1fa5481aecf3c8207259173b19
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-11-11 19:36:40 +0530

    Time: 1 ms (15.31%), Space: 43.5 MB (8.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,34 +5,23 @@ class Solution {
         int n = obstacleGrid[0].length;
 
         int[][] dp = new int[m][n];
-        for (int[] arr : dp){
-            Arrays.fill(arr, -1);
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else if (i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }else {
+                    int up = 0, left = 0;
+                    if (i > 0) up = dp[i - 1][j];
+                    if (j > 0) left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+            }
         }
-        return helper(obstacleGrid, m - 1, n - 1, dp);
+       
+        return dp[m - 1][n - 1];
         
     }
-
-     public int helper(int[][] arr, int m, int n, int[][] dp){
-
-            if (m == 0 && n == 0 && arr[m][n] == 0){
-                return 1;
-            }
-
-            if (m < 0 || n < 0){
-                return 0;
-            }
-
-            if (arr[m][n] == 1){
-                return 0;
-            }
-
-            if (dp[m][n] != -1){
-                return dp[m][n];
-            }
-
-            int up = helper(arr, m - 1, n, dp);
-            int left = helper(arr, m, n - 1, dp);
-
-            return dp[m][n] = up + left;
-        }
 }
