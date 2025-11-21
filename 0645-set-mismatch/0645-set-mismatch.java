ac36689e6fe0e9fe4cb6c2e08e8b51f3df71d99b
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-11-21 17:24:21 +0530

    Time: 1 ms (99.74%), Space: 47.3 MB (46.66%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
         int[] ans = new int[2];
-        for (int n : nums){
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        boolean[] seen = new boolean[n + 1];
+
+        for (int i : nums){
+            if (seen[i]){
+                ans[0] = i;
+            }
+            seen[i] = true;
         }
 
-        for (Map.Entry<Integer, Integer> m : map.entrySet()){
-            if (m.getValue() == 2){
-                ans[0] = m.getKey();
-            }  
-        }
-
-        for (int i = 1; i <= nums.length; i++){
-            if (!map.containsKey(i)){
+        for (int i = 1; i <= n; i++){
+            if(!seen[i]){
                 ans[1] = i;
+                break;
             }
         }
-
+    
         return ans;
     }
 }
