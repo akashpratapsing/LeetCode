f83f471176752dee1f01e3ccf1faa1da348c5124
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-12-31 16:43:48 +0530

    Time: 2 ms (72.51%), Space: 45.4 MB (38.95%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -2,33 +2,25 @@ class Solution {
     public int maxProfit(int k, int[] prices) {
 
         int n = prices.length;
-        int[][][] dp = new int[n][2][k + 1];
-        for (int[][] arrays : dp) {
-            for (int[] array : arrays) {
-                Arrays.fill(array, -1);
+        int[][][] dp = new int[n + 1][2][k + 1];
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int transactions = 1; transactions <= k; transactions++) {
+
+                    int profit = 0;
+                    if (buy == 1) {
+                        profit = Math.max(-prices[ind] + dp[ind + 1][0][transactions],
+                                dp[ind + 1][1][transactions]);
+                    } else {
+                        profit = Math.max(prices[ind] + dp[ind + 1][1][transactions - 1],
+                                dp[ind + 1][0][transactions]);
+                    }
+
+                    dp[ind][buy][transactions] = profit;
+                }
             }
         }
-        return f(prices, 0, 1, k, dp);
-    }
-
-    public int f(int[] arr, int ind, int buy, int transactions, int[][][] dp) {
-
-        if (transactions == 0)
-            return 0;
-        if (ind == arr.length)
-            return 0;
-        if (dp[ind][buy][transactions] != -1)
-            return dp[ind][buy][transactions];
-
-        int profit = 0;
-        if (buy == 1) {
-            profit = Math.max(-arr[ind] + f(arr, ind + 1, 0, transactions, dp),
-                    f(arr, ind + 1, 1, transactions, dp));
-        } else {
-            profit = Math.max(arr[ind] + f(arr, ind + 1, 1, transactions - 1, dp),
-                    f(arr, ind + 1, 0, transactions, dp));
-        }
-
-        return dp[ind][buy][transactions] = profit;
+        return dp[0][1][k];
     }
 }
