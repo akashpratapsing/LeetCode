54aab24a3bf3de343d348ae1ab981de648739dd5
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-03 17:19:23 +0530

    Time: 20 ms (78.24%), Space: 54 MB (32.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,21 +1,22 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        
+
         int n = text1.length();
         int m = text2.length();
-        int[][] dp = new int[n][m];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        for (int i = 0; i <= m; i++)
+            dp[0][i] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1))
+                     dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
-        return f(n - 1, m - 1, text1, text2, dp);
-    }
-
-    public int f(int i, int j, String s1, String s2, int[][] dp){
-
-        if (i < 0 || j < 0) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s1.charAt(i) == s2.charAt(j)) return dp[i][j] = 1 + f(i - 1, j - 1, s1, s2, dp);
-        return dp[i][j] = Math.max(f(i - 1, j, s1, s2, dp), f(i, j - 1, s1, s2, dp));
+        return dp[n][m];
     }
 }
