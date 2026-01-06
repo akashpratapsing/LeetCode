57ba702f8b38bb01badb173c4fa8ba29d75a75e1
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-06 15:02:18 +0530

    Time: 10 ms (34.6%), Space: 53 MB (90.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,31 +1,35 @@
 class Solution {
-
-    public void dfs(int row, int col, int[][] grid, boolean[][] visited){
-        visited[row][col] = true;
-
-        int[] dRow = {-1, 0, 1, 0};
-        int[] dCol = {0, 1, 0, -1};
-
-        for (int i = 0; i < 4; i++){
-            int nRow = row + dRow[i];
-            int nCol = col + dCol[i];
-
-            if (nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
-                !visited[nRow][nCol] && grid[nRow][nCol] == 1){
-                    dfs(nRow, nCol, grid, visited);
-                }
-        }
-    }
     public int numEnclaves(int[][] grid) {
         
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] visited = new boolean[n][m];
+        Queue<int[]> q = new LinkedList<>();
 
         for (int i = 0; i < n; i++){
             for (int j = 0; j < m; j++){
                 if ((i == 0 || j == 0 || i == n - 1 || j == m - 1) && grid[i][j] == 1){
-                    dfs(i, j, grid, visited);
+                    visited[i][j] = true;
+                    q.offer(new int[] {i, j});
+                }
+            }
+        }
+
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+        while (!q.isEmpty()){
+            int row = q.peek()[0];
+            int col = q.peek()[1];
+            q.poll();
+
+            for (int i = 0; i < 4; i++){
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
+
+                if (nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
+                !visited[nRow][nCol] && grid[nRow][nCol] == 1){
+                    visited[nRow][nCol] = true;
+                    q.offer(new int[] {nRow, nCol});
                 }
             }
         }
