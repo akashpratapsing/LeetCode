158d641a15fd8f7e4d2d06548c8ee30018d68152
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-09 17:57:41 +0530

    Time: 17 ms (81.66%), Space: 54.6 MB (22.87%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,11 +2,11 @@ class Solution {
 
     public int f(int i, int j, String s, String t, int[][] dp){
         
-        if (j < 0) return 1;
-        if (i < 0) return 0;
+        if (j == 0) return 1;
+        if (i == 0) return 0;
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (s.charAt(i) == t.charAt(j)) {
+        if (s.charAt(i - 1) == t.charAt(j - 1)) {
             return dp[i][j] = f(i - 1, j - 1, s, t, dp) + f(i - 1, j, s, t, dp);
         }
         return dp[i][j] = f(i - 1, j, s, t, dp);
@@ -15,10 +15,10 @@ class Solution {
         
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n + 1][m + 1];
         for (int[] arr : dp){
             Arrays.fill(arr, -1);
         }
-        return f(n - 1, m - 1, s, t, dp);
+        return f(n, m, s, t, dp);
     }
 }
