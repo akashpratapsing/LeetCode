709199ee0af85d62204d7f5152cfd3fa92136b85
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-11 19:46:44 +0530

    Time: 3 ms (96.62%), Space: 47.4 MB (8.99%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int f(int i, int j, String word1, String word2, int[][] dp) {
+
+        if (i < 0) return j + 1;
+        if (j < 0) return i + 1;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (word1.charAt(i) == word2.charAt(j))
+            return dp[i][j] = f(i - 1, j - 1, word1, word2, dp);
+
+        return dp[i][j] = 1 + Math.min(f(i - 1, j, word1, word2, dp),
+                Math.min(f(i, j - 1, word1, word2, dp),
+                        f(i - 1, j - 1, word1, word2, dp)));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n][m];
+        for (int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        return f(n - 1, m - 1, word1, word2, dp);
+    }
+}
