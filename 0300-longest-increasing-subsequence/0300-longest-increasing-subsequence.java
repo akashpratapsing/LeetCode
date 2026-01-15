b41219ec87f593b1c582d7208c452edcb9377671
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-16 02:01:32 +0530

    Time: 51 ms (35.41%), Space: 46.5 MB (31.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,26 +3,35 @@ class Solution {
 
         int n = nums.length;
         int[] dp = new int[n];
-        // int[] hash = new int[n];
+        int[] hash = new int[n];
         Arrays.fill(dp, 1);
-        // Arrays.fill(hash, -1);
-        int maxInd = 0;
-        int max = Integer.MIN_VALUE;
+        Arrays.fill(hash, -1);
+        int max = 1;
+        int lastInd = 0;
 
         for (int i = 0; i < n; i++){
             for (int prev = 0; prev < i; prev++){
 
-                if(nums[prev] < nums[i]){
-                    dp[i] = Math.max(dp[i], 1 + dp[prev]);
-                    // hash[i] = prev;
+                if(nums[prev] < nums[i] && 1 + dp[prev] > dp[i]){
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
                 }
             }
-            max = Math.max(max, dp[i]);
-            // if (nums[i] > max){
-            //     maxInd = i;
-            //     max = nums[i];
-            // }
+
+            if (dp[i] > max){
+                max = dp[i];
+                lastInd = i;
+            }
         }
+
+        List<Integer> lis = new ArrayList<>();
+        int curr = lastInd;
+        while (curr != -1){
+            lis.add(nums[curr]);
+            curr = hash[curr];
+        }
+        Collections.reverse(lis);
+        System.out.println(lis);
        
         return max;
     }
