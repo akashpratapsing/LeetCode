0536dc35e4d550d77b0ff9c5ce052addf7cc50a7
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-27 17:50:11 +0530

    Time: 96 ms (35.66%), Space: 48.8 MB (37.32%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,45 @@
+class Pair{
+    String first;
+    int second;
+
+    public Pair(String first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        Set<String> set = new HashSet<>();
+        for (String word : wordList){
+            set.add(word);
+        }        
+
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(beginWord, 1));
+
+        while (!q.isEmpty()){
+            String word = q.peek().first;
+            int steps = q.peek().second;
+            q.poll();
+
+            if (word.equals(endWord)) return steps;
+
+            for (int i = 0; i < word.length(); i++){
+                for (char ch = 'a'; ch <= 'z'; ch++){
+                    char[] replacedCharArr = word.toCharArray();
+                    replacedCharArr[i] = ch;
+                    String replacedWord = new String(replacedCharArr);
+
+                    if (set.contains(replacedWord)){
+                        set.remove(replacedWord);
+                        q.offer(new Pair(replacedWord, steps + 1));
+                    } 
+                }
+            }
+        }
+
+        return 0;
+    }
+}
