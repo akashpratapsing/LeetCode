de643ab7cddafd0791dc3cd64040aff204c0e588
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-28 16:29:59 +0530

    Time: 18 ms (38.06%), Space: 47.9 MB (35.42%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,12 +1,10 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length;
 
         if (grid[0][0] == 1)
             return -1;
-        if (n == 1)
-            return 1;
 
+        int n = grid.length;
         int[][] dist = new int[n][n];
         for (int[] a : dist) {
             Arrays.fill(a, (int) 1e9);
@@ -25,17 +23,16 @@ class Solution {
             int dis = q.peek()[0];
             q.poll();
 
+            if (row == n - 1 && col == n - 1){
+                return dis;
+            }
+
             for (int i = 0; i < 8; i++) {
                 int nRow = row + dRow[i];
                 int nCol = col + dCol[i];
 
                 if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < n &&
                         grid[nRow][nCol] == 0 && dis + 1 < dist[nRow][nCol]) {
-
-                    if (nRow == n - 1 && nCol == n - 1) {
-                        return dis + 1;
-                    }
-
                     dist[nRow][nCol] = dis + 1;
                     q.offer(new int[] { dis + 1, nRow, nCol });
                 }
