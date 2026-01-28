e5acd11e81959e5d6b478221f3e71984f9e629c3
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-28 16:27:58 +0530

    Time: 24 ms (21.28%), Space: 47.7 MB (47.71%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,42 +1,44 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        
-        if (grid[0][0] == 1) return -1;
-        if (grid.length == 1) return 1;
-
         int n = grid.length;
+
+        if (grid[0][0] == 1)
+            return -1;
+        if (n == 1)
+            return 1;
+
         int[][] dist = new int[n][n];
-        for (int[] a: dist){
+        for (int[] a : dist) {
             Arrays.fill(a, (int) 1e9);
         }
         dist[0][0] = 1;
-        
-        int[] dRow = {-1, -1, -1, 1, 1, 1, 0, 0};
-        int[] dCol = {-1, 0, 1, -1, 0, 1, -1, 1};
-        
+
+        int[] dRow = { -1, -1, -1, 1, 1, 1, 0, 0 };
+        int[] dCol = { -1, 0, 1, -1, 0, 1, -1, 1 };
+
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {1, 0, 0});
-        
-        while (!q.isEmpty()){
+        q.offer(new int[] { 1, 0, 0 });
+
+        while (!q.isEmpty()) {
             int row = q.peek()[1];
             int col = q.peek()[2];
             int dis = q.peek()[0];
             q.poll();
-            
-            for (int i = 0; i < 8; i++){
+
+            for (int i = 0; i < 8; i++) {
                 int nRow = row + dRow[i];
                 int nCol = col + dCol[i];
-                
+
                 if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < n &&
-                    grid[nRow][nCol] == 0 && dis + 1 < dist[nRow][nCol]){
-                        
-                        if (nRow == n - 1 && nCol == n - 1){
-                            return dis + 1;
-                        }
-                        
-                        dist[nRow][nCol] = dis + 1;
-                        q.offer(new int[] {dis + 1, nRow, nCol});
+                        grid[nRow][nCol] == 0 && dis + 1 < dist[nRow][nCol]) {
+
+                    if (nRow == n - 1 && nCol == n - 1) {
+                        return dis + 1;
                     }
+
+                    dist[nRow][nCol] = dis + 1;
+                    q.offer(new int[] { dis + 1, nRow, nCol });
+                }
             }
         }
         return -1;
