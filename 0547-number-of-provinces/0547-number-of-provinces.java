f187f7c0d678d8112545bec8b60b84f0b1d1c73c
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-02-06 17:38:55 +0530

    Time: 4 ms (20.36%), Space: 46.9 MB (96.59%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,39 +1,54 @@
-class Solution {
-
-    public void dfs(ArrayList<ArrayList<Integer>> graph, int node, boolean[] visited){
-
-        visited[node] = true;
-        
-        for (int i : graph.get(node)){
-            if (!visited[i]){
-                dfs(graph, i, visited);
-            }
+class DisjointSet {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);
+            size.add(1);
         }
     }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
+class Solution {
+
     public int findCircleNum(int[][] isConnected) {
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         int n = isConnected.length;
-
-        for (int i = 0; i <= n; i++){
-            graph.add(new ArrayList<>());
-        }
+        DisjointSet ds = new DisjointSet(n);
 
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                if (isConnected[i][j] == 1 && i != j)
-                graph.get(i).add(j);
+                if (isConnected[i][j] == 1){
+                    ds.unionBySize(i, j);
+                }
             }
         }
 
-        boolean[] visited = new boolean[n + 1];
         int count = 0;
-
         for (int i = 0; i < n; i++){
-            if (!visited[i]){
-                dfs(graph, i, visited);
-                count++;
-            }
+            if (ds.parent.get(i) == i) count++;
         }
         return count;
     }
