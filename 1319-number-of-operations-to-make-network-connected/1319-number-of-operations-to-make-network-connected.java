1f15a3493a00a0d9c42e9b0c672f3ccc1ec25917
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-02-06 21:20:39 +0530

    Time: 15 ms (27.25%), Space: 70 MB (26.05%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,54 @@
+class DisjointSet {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        
+        int totalCables = connections.length;
+        if (totalCables < n - 1) return -1;
+
+        DisjointSet ds = new DisjointSet(n);
+        for (int[] conn: connections){
+            int u = conn[0];
+            int v = conn[1];
+            ds.unionBySize(u, v);
+        }
+
+        int components = 0;
+        for (int i = 0; i < n; i++){
+            if (ds.parent.get(i) == i) components++;
+        }
+
+        return components - 1;
+    }
+}
